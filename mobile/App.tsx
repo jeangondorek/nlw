a6de9ce86363cf163f6fc6d1bf7b770b40bd5565
jeangondorek <jeangondorek73@gmail.com>
2022-09-17 03:35:59 -0300

    app initialized

---
+++ mobile/App.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>EAE PORRA</Text>
+      <Button title='send 1'></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+     <Text>{props.title}</Text> 
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
