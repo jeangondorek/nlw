03f0f076b4e0ecbaacee278e5ed6ec18e941d74d
jeangondorek <jeangondorek73@gmail.com>
2022-09-18 23:29:24 -0300

    mobile interface

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,33 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {StatusBar} from 'react-native';
+import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter';
+import { Home } from './src/screens/Home';
+
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>EAE PORRA</Text>
-      <Button title='send 1'></Button>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      {fontsLoaded ? <Home/> : <Loading/>}
+    </Background>
   );
 }
-
-interface ButtonProps{
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return (
-    <TouchableOpacity>
-     <Text>{props.title}</Text> 
-    </TouchableOpacity>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
